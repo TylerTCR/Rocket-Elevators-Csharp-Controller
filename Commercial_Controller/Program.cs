d142098a938860c708dc0ec6a8f5a17309d21a0c
TylerTCR <tcil112000@gmail.com>
2021-10-22 19:53:18 -0400

    Removed unneeded comments

--- Commercial_Controller/Program.cs
+++ Commercial_Controller/Program.cs
@@ -5,19 +5,11 @@ namespace Commercial_Controller
 {
     class Program
     {
-        public int columnID = 1;
-        public int elevatorID = 1;
-        public int FloorRequestButtonID = 1;
-        public int callButtonID = 1;
-
         static void Main(string[] args)
         {
             int scenarioNumber = Int32.Parse(args[0]);
             Scenarios scenarios = new Scenarios();
             scenarios.run(scenarioNumber);
-            //List<int> floors = new List<int> {1, 2,3};
-            //Column c1 = new Column("1", 2, floors, false);
-
         }
     }
 }
